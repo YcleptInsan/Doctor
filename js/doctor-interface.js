b424208742410394d7646a5eb71d8b4b28097026
Nick Wise <nickmwise@gmail.com>
2017-06-30 12:42:30 -0700

    Add append section to doctorInterface and extra arguments to include doctors firstName lastName title and bio. Work on getting data from api to display a list of doctors and their info.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,13 @@
-const apiKey = require('./../.env').apiKey;
 const Doctor = require('./../js/doctor.js');
-$(document).ready(function() {
+
+var displayDoctors = function (medicalIssue, firstName, lastName, title, bio) { $('#showDoctors').append('<li> firstName + " " + lastName + " " + title + " " bio + " " + </li> ');
+};
+$(() => {
   $("#search-button").click(function() {
+    var doctor = new Doctor();
     var medicalIssue = $("#user-search").val();
-    $("user-search").val("");
+    $("#user-search").val("");
+    doctor.getDoctors(medicalIssue, displayDoctors);
     console.log(medicalIssue);
   });
-    var doctor = new Doctor();
-    doctor.getDoctors(medicalIssue);
-  });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -1,19 +1,16 @@
-class Doctor {
-  constructor() {
-    
-  }
+const apiKey = require('./../.env').apiKey;
+function Doctor() {
 }
 
-
-  Doctor.prototype.getDoctors = function(medicalIssue) {
+  Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-    .then(function(result) {
+    .then(result)
 
-    console.log(result);
-  })
+    var displayDoctors(medicalIssue) 
+
     .fail(function(error){
-    console.log("fail");
-    });
+    $(".#showDoctors")(error.responseJSON.message);
+    );
 };
 
 exports.doctorModule = Doctor;
