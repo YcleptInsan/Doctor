c4c8f6f97a5a75a805f54fd81951230949c71d30
Nick Wise <nickmwise@gmail.com>
2017-06-30 18:06:32 -0700

    Error still having issue with display

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,15 +1,14 @@
-var Doctor = require('./../js/doctor.js');
+var Doctor = require('./../js/doctor.js').doctorModule;
 
-var displayDoctors = function (medicalIssue, firstName, lastName, title, bio) { $('#showDoctors').append('<li> + firstName + " " + lastName + " " + title + " " bio + " " + <br> </li>');
+var displayDoctor = function(medicalIssue, firstName, lastName, title, bio) {
+    $('.showDoctors').append('<li>' + '"><br><h4>'+ firstName + " " + lastName + "," + title + "</h4>" + bio + "</li><br><hr>");
 };
-$(() => {
-
+$(document).ready(function() {
   var doctor = new Doctor();
-
-  $("#search-button").click(function() {
-    $("#showDoctors").empty();
-    var medicalIssue = $("#user-search").val();
-    $("#user-search").val("");
-    doctor.getDoctors(medicalIssue, displayDoctors);
+  $('#search-button').click(function() {
+    $(".showDoctors" ).empty();
+    var medicalIssue = $('#issue').val();
+    $('#user-search').val("");
+    doctor.getDoctor(medicalIssue, displayDoctor);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -3,15 +3,19 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {
 }
   //method to get the targeted information from api
-  Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
+  Doctor.prototype.getDoctor = function(medicalIssue, displayDoctors) {
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
 
     .then(function(response) {
-      for(var i = 0; i <= response.data.length; i++){
-      displayDoctors(medicalIssue, response.data[i].profile.first_name, response.data[i].profile.last_name, response.data[i].profile.title, response.data[i].profile.bio);
+      for(var i = 0; i <= response.data.length ; i++){
+      displayDoctor(medicalIssue,
+        response.data[i].profile.first_name, response.data[i].profile.last_name, response.data[i].profile.title,
+        response.data[i].profile.bio);
+        console.log(JSON.stringify(response));
       }
     })
-    .fail(function(error){ $("#showDoctors").text(error.responseJSON.message);
+    .fail(function(error){
+      $(".showDoctors").text(error.responseJSON.message);
   });
 };
 
